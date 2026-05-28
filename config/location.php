8cd8a651b44eec2c2ca71f5e9cec1bdabe73c437
Marc González Majoral <gonssal@gmail.com>
2026-05-28 13:32:33 +0200

    Normalize pahts to storage/app instead.
    This is git-ignored by default in Laravel, so no risk to accidentally commit big downloaded files.

--- config/location.php
+++ config/location.php
@@ -135,7 +135,7 @@ return [
 
         'local' => [
             'type' => 'city',
-            'path' => env('MAXMIND_DATABASE_PATH', storage_path('location/maxmind/GeoLite2-City.mmdb')),
+            'path' => env('MAXMIND_DATABASE_PATH', storage_path('app/location/maxmind/GeoLite2-City.mmdb')),
             // The filesystem disk to store the database file on at the configured path.
             // When null, the local disk will be used by default.
             'disk' => env('MAXMIND_DATABASE_DISK'),
